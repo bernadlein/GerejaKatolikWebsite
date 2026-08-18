0f3e76952ce40e156364527e014d8095af33cdad
bernadlein <108117547+bernadlein@users.noreply.github.com>
2026-08-18 18:38:40 +0700

    Update useWeeklyMassSchedule.ts

--- app/composables/useWeeklyMassSchedule.ts
+++ app/composables/useWeeklyMassSchedule.ts
@@ -1,12 +1,40 @@
-import { getWeeklyMassSchedule } from '~/application/mass/getWeeklyMassSchedule'
-import { staticMassRepository } from '~/infrastructure/mass/staticMassRepository'
+import {
+  getWeeklyMassSchedule,
+} from '~/application/mass/getWeeklyMassSchedule'
+
+import {
+  staticMassRepository,
+} from '~/infrastructure/mass/staticMassRepository'
+
+import {
+  presentWeeklyMassSchedule,
+} from '~/presenters/mass/presentWeeklyMassSchedule'
 
 export function useWeeklyMassSchedule() {
-  return useAsyncData(
+  const {
+    data,
+    status,
+    error,
+    refresh,
+  } = useAsyncData(
     'weekly-mass-schedule',
     () =>
       getWeeklyMassSchedule(
         staticMassRepository,
       ),
   )
+
+  const schedules = computed(
+    () =>
+      presentWeeklyMassSchedule(
+        data.value ?? [],
+      ),
+  )
+
+  return {
+    schedules,
+    status,
+    error,
+    refresh,
+  }
 }
